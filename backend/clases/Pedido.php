02e190bfd34f0bb5312f6c3730db0e568f8bace2
agmorelli <augustomorelli206@gmail.com>
2018-06-22 20:39:51 -0300

    clase producto

--- backend/clases/Pedido.php
+++ backend/clases/Pedido.php
@@ -16,24 +16,24 @@ public $fotoMesa;
 public function GuardarPedido()
 {
  
-                $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-				$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into pedidos (idMesa, tiempoInicio, fotoMesa)values(:idMesa, :tiempoInicio, :fotoMesa)");
-                $consulta->bindValue(':idMesa', $this->idMesa, PDO::PARAM_INT);
-                $consulta->bindValue(':tiempoInicio', $this->tiempoInicio, PDO::PARAM_STR);
-                $consulta->bindValue(':fotoMesa', $this->fotoMesa, PDO::PARAM_STR);
-                $consulta->execute();
-				return $objetoAccesoDato->RetornarUltimoIdInsertado();
+    $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+	$consulta =$objetoAccesoDato->RetornarConsulta("INSERT into pedidos (idMesa, tiempoInicio, fotoMesa)values(:idMesa, :tiempoInicio, :fotoMesa)");
+    $consulta->bindValue(':idMesa', $this->idMesa, PDO::PARAM_INT);
+    $consulta->bindValue(':tiempoInicio', $this->tiempoInicio, PDO::PARAM_STR);
+    $consulta->bindValue(':fotoMesa', $this->fotoMesa, PDO::PARAM_STR);
+    $consulta->execute();
+	return $objetoAccesoDato->RetornarUltimoIdInsertado();
 }
 
 
 public static function TraerTodosLosPedidos() 
 {
-			$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
-			$consulta =$objetoAccesoDato->RetornarConsulta("SELECT * from pedidos ");  
-			$consulta->execute();
-			$pedidos= $consulta->fetchAll(PDO::FETCH_CLASS, "Pedido");
+	$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+	$consulta =$objetoAccesoDato->RetornarConsulta("SELECT * from pedidos ");  
+	$consulta->execute();
+	$pedidos= $consulta->fetchAll(PDO::FETCH_CLASS, "Pedido");
             
-            return $pedidos;
+    return $pedidos;
 									
 }
 
